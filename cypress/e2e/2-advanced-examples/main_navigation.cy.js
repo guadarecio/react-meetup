d907edd3c8535014df7f2d7e847f8ff9f852649f
g.recio <g.recio@proun.es>
2024-01-23 17:36:30 +0100

    main navigation cypress test e2e

---
+++ cypress/e2e/2-advanced-examples/main_navigation.cy.js
@@ -0,0 +1,47 @@
+/// <reference types="cypress" />
+
+context("React Meetups", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/#all-meetups");
+  });
+
+  it("Click in All Meetups", () => {
+    cy.get(":nth-child(1) > a").click();
+    cy.wait(1500);
+    cy.get(
+      ":nth-child(1) > .Card_card__48SLx > .MeetupItem_actions__mhVrb > button"
+    ).click();
+    cy.wait(5000);
+    cy.get(
+      ":nth-child(1) > .Card_card__48SLx > .MeetupItem_actions__mhVrb > button"
+    ).click();
+  });
+
+  it("Click in All Meetups and complete de form", () => {
+    cy.get(":nth-child(2) > a").click();
+    cy.get("#title")
+      .type("Stand-up meetup")
+      .should("have.value", "Stand-up meetup");
+    cy.get("#image")
+      .type(
+        "https://resulta-2.com/wp-content/uploads/innovacion-en-equipos.png"
+      )
+      .should(
+        "have.value",
+        "https://resulta-2.com/wp-content/uploads/innovacion-en-equipos.png"
+      );
+    cy.get("#address")
+      .type("La creu Roja, 1")
+      .should("have.value", "La creu Roja, 1");
+
+    cy.get("#description")
+      .type("Standup semanal y comienzo de sprint")
+      .should("have.value", "Standup semanal y comienzo de sprint");
+
+    cy.get("button").click();
+  });
+
+  it("Click in My favorites", () => {
+    cy.get(":nth-child(3) > a").click();
+  });
+});
