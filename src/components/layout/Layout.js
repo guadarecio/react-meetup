70a59640505dca294e3a435485095d76b66dd582
g.recio <g.recio@proun.es>
2024-01-23 11:21:47 +0100

    animation added to header

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,7 +1,6 @@
+import classes from "./Layout.module.css";
 
-import classes from './Layout.module.css';
-
-export default function Layout({children}) {
+export default function Layout({ children }) {
   return (
     <div>
       <main className={classes.main}>{children}</main>
--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import {
   ALL_MEETUP_PAGE,
   FAVORITES_PAGE,
@@ -7,24 +8,45 @@ import {
 import classes from "./MainNavigation.module.css";
 
 export default function MainNavigation({ setPage }) {
+  const [prevScrollPos, setPrevScrollPos] = useState(window.pageYOffset);
+  const [visible, setVisible] = useState(true);
+
+  const handleScroll = () => {
+    const currentScrollPos = window.pageYOffset;
+    setVisible(prevScrollPos > currentScrollPos || currentScrollPos < 50);
+    setPrevScrollPos(currentScrollPos);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [prevScrollPos, visible]);
+
   return (
-    <header className={classes.header} data-test="navigation-header">
+    <header
+      className={`${classes.header} ${
+        visible ? classes.visible : classes.hidden
+      }`}
+      data-test="navigation-header"
+    >
       <div className={classes.logo}>React Meetups</div>
       <nav>
         <ul>
           <li>
-            <a href="#all-meetups" onClick={() => setPage(ALL_MEETUP_PAGE)}>
+            <a href="/#all-meetups" onClick={() => setPage(ALL_MEETUP_PAGE)}>
               All Meetups
             </a>
           </li>
 
           <li>
-            <a href="#new-meetup" onClick={() => setPage(NEW_MEETUP_PAGE)}>
+            <a href="/#new-meetup" onClick={() => setPage(NEW_MEETUP_PAGE)}>
               Add New Meetup
             </a>
           </li>
           <li>
-            <a href="#my-favorites" onClick={() => setPage(FAVORITES_PAGE)}>
+            <a href="/#my-favorites" onClick={() => setPage(FAVORITES_PAGE)}>
               My Favorites
               <span className={classes.badge}>{0}</span>
             </a>
