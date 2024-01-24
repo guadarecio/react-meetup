da33ac8862cfa221d7a9ccd39acb871973bbc46e
g.recio <g.recio@proun.es>
2024-01-24 15:26:11 +0100

    final changes in cypress tests

--- cypress/e2e/main_navigation.cy.js
+++ cypress/e2e/main_navigation.cy.js
@@ -1,26 +1,20 @@
 /// <reference types="cypress" />
 
 context("React Meetups", () => {
-  beforeEach(() => {
+  it("should show header animation on scroll, add favorite and navigate to it", () => {
     cy.visit("http://localhost:3000/#all-meetups");
-  });
-
-  it("Click in All Meetups", () => {
     cy.scrollTo("bottom", { duration: 2000 });
     cy.scrollTo("top", { duration: 1000 });
-    cy.get(":nth-child(1) > a").click();
     cy.wait(1500);
     cy.get(
       ":nth-child(1) > .Card_card__48SLx > .MeetupItem_actions__mhVrb > button"
     ).click();
-    cy.wait(5000);
-    cy.get(
-      ":nth-child(1) > .Card_card__48SLx > .MeetupItem_actions__mhVrb > button"
-    ).click();
-    cy.wait(1500);
+    cy.visit("http://localhost:3000/#my-favorites");
+    cy.get(":nth-child(3) > a").click();
   });
 
-  it("Click in All Meetups and complete de form", () => {
+  it("should add new meetup and show it in the homepage", () => {
+    cy.visit("http://localhost:3000/#new-meetup");
     cy.get(":nth-child(2) > a").click();
     cy.get("#title")
       .type("Stand-up meetup")
@@ -42,9 +36,7 @@ context("React Meetups", () => {
       .should("have.value", "Standup semanal y comienzo de sprint");
 
     cy.get("button").click();
-  });
-
-  it("Click in My favorites", () => {
-    cy.get(":nth-child(3) > a").click();
+    cy.get(":nth-child(1) > a").click();
+    cy.scrollTo("bottom", { duration: 3000 });
   });
 });
